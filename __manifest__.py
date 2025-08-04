42ebd2e347d095de6e04f545646736e1566460af
Moaz Elbahr <moazelbahr@gmail.com>
2025-08-04 17:15:34 +0300

    Before and after review

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,19 @@
+# -*- coding: utf-8 -*-
 {
-    'name': "Account Automatic Transfer Daily",
-    'description': "Account Automatic Transfer Daily",
-    'author' : "Moaz Elbahr",
-    'category': '',
-    'version': '18.0.0.1.0',
+    'name': "Account Auto Transfer",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'author': "CDS Solutions SRL",
+    'website': "www.cdsegypt.com",
+    'contributors': [
+        'Ramadan Khalil <rkhalil1990@gmail.com>',
+    ],
+    'version': '0.1',
     'depends': ['accountant', 'account'],
-    'data': []
+    'data': [],
+    'license': 'OPL-1',
+    "pre_init_hook": None,
+    "post_init_hook": None,
 }
--- models/account_transfer_model_inherit.py
+++ models/account_transfer_model_inherit.py
@@ -1,14 +1,35 @@
+# I imported this at the beginning because I was going to use the same way as the original function
+# But after reviewing I found the other way so I deleted this import line
+from dateutil.relativedelta import relativedelta
 from odoo import models, fields, api
 
 class AccountAutomaticTransfer(models.Model):
     _inherit = 'account.transfer.model'
 
-    frequency = fields.Selection(selection_add=[('daily', 'Daily')], ondelete={'daily': 'set default'})
+# I didn't know it was possible to inherit and just add a new selection at the beginning so I just used this way
+    frequency = fields.Selection([
+        ('daily', 'Daily'),
+        ('month', 'Monthly'),
+        ('quarter', 'quarterly'),
+        ('year', 'yearly')
+    ])
+# This is the way I found after reviewing the code with
+    # frequency = fields.Selection(selection_add=[('daily', 'Daily')], ondelete={'daily': 'set default'})
 
-
-    def _get_next_move_date(self, current_date):
+# Here I debugged till I found the function name and inherited it and added this
+    def _get_next_move_date(self, date):
         self.ensure_one()
-
         if self.frequency == 'daily':
-            return current_date
-        return super()._get_next_move_date(current_date)
+# This is the same way as the original function but I replaced it later when I found the other way
+            delta = relativedelta(days=1)
+        else:
+            return super()._get_next_move_date(date)
+        return date + delta - relativedelta(days=1)
+
+# This is the way I found after review
+    # def _get_next_move_date(self, current_date):
+    #     self.ensure_one()
+    #
+    #     if self.frequency == 'daily':
+    #         return current_date
+    #     return super()._get_next_move_date(current_date)
